e01f6795fac74c4a95d07322ddd4b0057e87c992
Datt <datt1994@gmail.com>
2020-08-03 17:52:44 +0530

    Add loader for view

--- ApiCallWithDecodable/ApiCallWithDecodable/ApiCall.swift
+++ ApiCallWithDecodable/ApiCallWithDecodable/ApiCall.swift
@@ -16,26 +16,34 @@ public enum ApiCallResult<Value,ResponseError : GeneralResponseModel> {
 
 public class ApiCall: NSObject {
     
-    let constValueField = "application/json"
-    let constHeaderField = "Content-Type"
+    private let constValueField = "application/json"
+    private let constHeaderField = "Content-Type"
     
-    public func post<T : Decodable ,A>(apiUrl : String, params: [String: A], model: T.Type , completion: @escaping (ApiCallResult<T,GeneralResponseModel>) -> ()) {
-        requestMethod(apiUrl: apiUrl, params: params as [String : AnyObject], method: "POST", model: model, completion: completion)
+    private var observationLoaderView: NSKeyValueObservation?
+    
+    public func post<T : Decodable ,A>(apiUrl : String, params: [String: A], model: T.Type, loaderInView : UIView? = nil, completion: @escaping (ApiCallResult<T,GeneralResponseModel>) -> ()) {
+        requestMethod(apiUrl: apiUrl, params: params as [String : AnyObject], method: "POST", model: model, loaderInView : loaderInView, completion: completion)
     }
     
-    public func get<T : Decodable>(apiUrl : String, model: T.Type , completion: @escaping (ApiCallResult<T,GeneralResponseModel>) -> ()) {
-        requestMethod(apiUrl:apiUrl, params: [:], method: "GET",model: model,  completion: completion)
+    public func get<T : Decodable>(apiUrl : String, model: T.Type, loaderInView : UIView? = nil, completion: @escaping (ApiCallResult<T,GeneralResponseModel>) -> ()) {
+        requestMethod(apiUrl:apiUrl, params: [:], method: "GET",model: model, loaderInView : loaderInView, completion: completion)
     }
     
-    public func put<T : Decodable ,A>(apiUrl : String, params: [String: A], model: T.Type , completion: @escaping (ApiCallResult<T,GeneralResponseModel>) -> ()) {
-        requestMethod(apiUrl:apiUrl, params: params as [String : AnyObject], method: "PUT",model: model,  completion: completion)
+    public func put<T : Decodable ,A>(apiUrl : String, params: [String: A], model: T.Type, loaderInView : UIView? = nil, completion: @escaping (ApiCallResult<T,GeneralResponseModel>) -> ()) {
+        requestMethod(apiUrl:apiUrl, params: params as [String : AnyObject], method: "PUT",model: model, loaderInView : loaderInView, completion: completion)
     }
     
-    public func requestMethod<T : Decodable>(apiUrl : String, params: [String: AnyObject], isToken: Bool = true, method: String, model: T.Type  , completion: @escaping (ApiCallResult<T,GeneralResponseModel>) -> ()) {
+    public func requestMethod<T : Decodable>(apiUrl : String, params: [String: AnyObject], isToken: Bool = true, method: String, model: T.Type, loaderInView : UIView? = nil, completion: @escaping (ApiCallResult<T,GeneralResponseModel>) -> ()) {
+        
+        var loaderView : UIView?
+        if var view = loaderInView {
+            self.addLoaderInView(&view, loaderView:&loaderView)
+        }
         
         var request = URLRequest(url: URL(string: apiUrl)!)
         request.httpMethod = method
         request.setValue(constValueField, forHTTPHeaderField: constHeaderField)
+        //request.setValue(strToken, forHTTPHeaderField: "Authorization")
         
         if !params.isEmpty {
             let jsonTodo: NSData
@@ -82,8 +90,41 @@ public class ApiCall: NSObject {
                     completion(.error(error))
                 }
             }
+            
+            if let loaderView = loaderView {
+                mainThread { loaderView.removeFromSuperview() }
+            }
         })
         task.resume()
+    }
+    
+    func addLoaderInView(_ view: inout UIView, loaderView : inout UIView?) {
+        loaderView = UIView(frame: view.bounds)
+        loaderView?.backgroundColor = getBGColor(view)
+        let activityIndicator = UIActivityIndicatorView()
+        if #available(iOS 13.0, *) {
+            activityIndicator.style = .large
+        } else {
+            activityIndicator.style = .whiteLarge
+        }
+        activityIndicator.color = .gray
+        activityIndicator.startAnimating()
+        loaderView?.addSubview(activityIndicator)
+        activityIndicator.anchorCenterSuperview()
+        view.addSubview(loaderView!)
+        observationLoaderView = view.observe(\UIView.bounds, options: .new) { [weak loaderView] view, change in
+            if let value =  change.newValue {
+                loaderView?.frame = value
+            }
+        }
+        //        loaderView?.anchorCenterSuperview()
+        //                loaderView?.fillToSuperview()
+    }
+    func getBGColor(_ view : UIView?) -> UIColor {
+        guard let view = view , let bgColor = view.backgroundColor else {
+            return .white
+        }
+        return bgColor == .clear ? getBGColor(view.superview) : bgColor
     }
     
 }
@@ -96,4 +137,36 @@ fileprivate func mainThread(_ completion: @escaping () -> ()) {
 public class GeneralResponseModel : Decodable {
     var message : String?
     var status : Int?
+}
+
+// MARK: Constraints Extensions
+fileprivate extension UIView {
+    /// : Anchor center X into current view's superview with a constant margin value.
+    ///
+    /// - Parameter constant: constant of the anchor constraint (default is 0).
+    @available(iOS 9, *) func anchorCenterXToSuperview(constant: CGFloat = 0) {
+        //
+        translatesAutoresizingMaskIntoConstraints = false
+        if let anchor = superview?.centerXAnchor {
+            centerXAnchor.constraint(equalTo: anchor, constant: constant).isActive = true
+        }
+    }
+    
+    /// : Anchor center Y into current view's superview with a constant margin value.
+    ///
+    /// - Parameter withConstant: constant of the anchor constraint (default is 0).
+    @available(iOS 9, *) func anchorCenterYToSuperview(constant: CGFloat = 0) {
+        //
+        translatesAutoresizingMaskIntoConstraints = false
+        if let anchor = superview?.centerYAnchor {
+            centerYAnchor.constraint(equalTo: anchor, constant: constant).isActive = true
+        }
+    }
+    
+    /// : Anchor center X and Y into current view's superview
+    @available(iOS 9, *) func anchorCenterSuperview() {
+        //
+        anchorCenterXToSuperview()
+        anchorCenterYToSuperview()
+    }
 }
